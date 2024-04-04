40bc0e1ebbb67ed9f0da5bb8acc928c8216e5f8d
naveenmehtaison <naveenmehta2024@outlook.com>
2024-04-05 00:45:09 +0530

    Adding_Form

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -1,20 +1,37 @@
-import React from "react"
-import classes from './Meals.module.css'
-const Meals=()=>{
-    const Dummy_arr = [{Name:'Chicken Biryani', Des:'Bringing the special taste of Hyderabad', Price:'Rs 70'},{Name:'Kadai Paneer',Des:"Veg Special",Price:'Rs 300'}]
+import React from "react";
+import classes from './Meals.module.css'; // Import CSS module for Meals component
+import classes2 from './Button.module.css'; // Import CSS module for Button component
+
+const Meals = () => {
+    const Dummy_arr = [
+        {Name:'Chicken Biryani', Des:'Bringing the special taste of Hyderabad', Price:'Rs 70'},
+        {Name:'Kadai Paneer', Des:"Veg Special", Price:'Rs 300'}
+    ];
+
     return(
         <div className={classes.header2}>
             {Dummy_arr.map((ele,i)=>(
-                <div>
-                    <h3>{ele.Name}</h3> 
-                    <p>{ele.Des}</p>
-                    <p style={{color:'red'}}>{ele.Price} </p> 
-                    <hr></hr>
-                </div>
+                <div key={i}>
 
+                    <div>
+                        
+                        <label htmlFor="input" style={{float:'right'}}>Amount</label>
+                        <br></br>
+                        <input id='input' style={{float:'right'}} type='number'/>
+                        <h3>{ele.Name}</h3> 
+                        <p>{ele.Des}</p>
+                        <p style={{color:'red'}}>{ele.Price}</p> 
+                        <button className={classes2.button2} align='right'>+Add</button>
+
+                    </div>
+
+                <hr />
+  
+                </div>
+                
             ))}
         </div>
-    )
-
+    );
 }
-export default Meals
+
+export default Meals;
